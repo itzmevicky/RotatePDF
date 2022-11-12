820f6050bb2f64fb37177bb377d63330b93a4e26
itzmevicky <mepillai98@gmail.com>
2022-11-12 20:31:40 +0530

    returns path location

--- Rotate/views.py
+++ Rotate/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render
 import PyPDF2
-from rest_framework import views
 from rest_framework.response import Response
-from rest_framework.parsers import FileUploadParser
 from .serializer import *
-
 from rest_framework import generics
-
+import os
 
 class PDF(generics.GenericAPIView):
     serializer_class =  PDFRotateSerializer
@@ -19,15 +15,14 @@ class PDF(generics.GenericAPIView):
 
         File = request.FILES.get('Upload_File')
         rotation = str(request.data['Rotation']).split(' ')[0]
-        pagenumber = request.data['Page_Number_To_Rotate']
-
+        pagenumber = request.data['Page_Number_To_Rotate']            
+        
         if not bool(File):
             return Response('File Missig :( ')
 
-
-
         if (bool(rotation) or bool(pagenumber))  == False:
             return Response('Either Rotation or Pagenumber Missig :( ')
+
 
         if str(File).split('.')[1] == 'pdf' :
             pdfReader = PyPDF2.PdfFileReader(File)
@@ -44,8 +39,10 @@ class PDF(generics.GenericAPIView):
             with open(str(File),'wb') as pr:
                 pdfWriter.write(pr)      
 
-            return Response('File Updated..... :)')
+            path = os.path.abspath(str(File))
+            
 
+            return Response(f'File Stored Successfully : {path}')
         return Response('Invalid File Format')
         
 
